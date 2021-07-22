234a2bde43ca394287efd2bc44eb2179ec0f62e4
Jsheumaker1 <Jsheumaker1@gmail.com>
2021-07-22 10:05:46 -0500

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+    "Hello, programmer!"
+end
+
+def greet (name)
+    puts "Hello, #{name}!"
+    "Hello, #{name}!"
+end
+
+greet("Naureen")
+
+def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+    "Hello, #{name}!"
+end
+
+def add (a, b)
+    (a + b)
+end
+
+def halve (c)
+    return (c / 2)
+end
+    
